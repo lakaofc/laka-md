96e54d3965df105ed18c93f7d9ff53ed214036e4
Sashikals <Sashikapiyarthna@gmail.com>
2025-01-27 21:16:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=x0IX0bbA#yLdVvWJWOXJ8J9RCcCc2AZoRIDcr9QXHq2nw56JQ9YE",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
-MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
+MONGODB: process.env.MONGODB || "mongodb://mongo:EWqTPBAVNiVNkcABpGXbUTtTnjktWoYO@autorack.proxy.rlwy.net:37869",
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
