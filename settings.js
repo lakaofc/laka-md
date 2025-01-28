6ca6e6cd5cb7d6318a0e4564ec3205a72c522100
MrMadu <pathum20082008@gmail.com>
2025-01-28 22:00:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,11 +7,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=B44RUBTb#Vd8abc9JEMiR9N85LlY_ySfvwdoRV72TR176SHPRo8g",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
 MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
 PREFIX: process.env.PREFIX || '.' ,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94761130892' : process.env.OWNER_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '94761130892' : process.env.DELETEMSGSENDTO
 };
